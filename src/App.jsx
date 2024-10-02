21c4bbaf93dd71f4d85b7ac853c93f59f73552f9
athervisingh <singhathervi001@gmail.com>
2024-10-03 04:23:46 +0530

    video

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='w-full'>
+        <iframe width="1600" height="800" src="https://www.youtube.com/embed/rgcJhBiA30A?si=4K52R2018me294ad" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+    
+    </>
+  )
+}
+
+export default App
