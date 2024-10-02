a62abd456b752a52f13ac6c6b883ff74d8d5caad
athervisingh <singhathervi001@gmail.com>
2024-10-03 04:34:59 +0530

    video1

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div className='w-full'>
+      <h1 className='items-center w-full flex justify-center text-6xl mt-6'>SIH 2024 - 1735 <sup>*</sup></h1>
+      <div className='w-full flex justify-center items-center mt-10'>
+        
         <iframe width="1600" height="800" src="https://www.youtube.com/embed/rgcJhBiA30A?si=4K52R2018me294ad" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
     
