12f0d9e35bc990a3157126279f274c30cb583761
athervisingh <singhathervi001@gmail.com>
2024-10-10 02:27:08 +0530

    add new vid

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,11 @@ function App() {
 
   return (
     <>
-      <h1 className='items-center w-full flex justify-center text-6xl mt-6'>SIH 2024 - 1735 <sup>*</sup></h1>
-      <div className='w-full flex justify-center items-center mt-10'>
+      <h1 className='items-center w-full flex justify-center text-4xl mt-6 max-sm:text-2xl'>SIH 2024 - 1735 <sup>*</sup></h1>
+      <h2 className='flex justify-center items-center mt-3 text-2xl font-bold font-mono  underline'>Semantic Segmentation of Satellite Imagery</h2>
+      <div className='w-full flex justify-center items-center mt-6'>
         
-        <iframe width="1600" height="800" src="https://www.youtube.com/embed/rgcJhBiA30A?si=4K52R2018me294ad" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe width="1600" height="670" src="https://www.youtube.com/embed/988bMjsHkpc?si=KzvxXTamAeFHoApn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
     
     </>
